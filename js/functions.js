fa55d31c6a65debe10512c68e6ea2d60147f13f1
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-17 16:23:45 +0100

    init

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+function createBox(n, where) {
+    
+    for (let i = 1; i < n + 1; i++ ){
+
+        document.getElementById(where).innerHTML += `
+            <div class="box">${i}</div>
+        `;
+    }
+}
+
+function clearBox(where) {
+
+    document.getElementById(where).innerHTML = ``;
+    console.clear;
+
+}
+/* function addClassOnClick(){
+   
+    this.classList.toggle("clicked");
+   
+    
+} */
---
+++ js/main.js
@@ -0,0 +1,25 @@
+let inizio = document.getElementById("start")
+
+inizio.addEventListener("click", 
+
+    clearBox("container"),
+    createBox(100, "container")
+
+    
+)
+let scatole = document.getElementsByClassName("box");
+
+for (const box of scatole) {
+    box.addEventListener('click', function onClick() {
+
+        for(i = 1; i < scatole.length; i++){
+
+            this.classList.toggle("clicked");
+
+        }   
+        console.log(i)
+    });
+} 
+
+
+
